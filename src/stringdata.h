fb5e34993d8c81ef78dee14cad7b81888a04c71f
Dedmen Miller <dedmen@dedmen.de>
2019-04-07 17:33:19 +0200

    Fix stringdata parsing

--- src/stringdata.h
+++ src/stringdata.h
@@ -6,21 +6,21 @@
 
 namespace sqf
 {
-	class stringdata : public data
-	{
-	private:
-		std::string mvalue;
-	public:
-		stringdata() : mvalue(std::string()) {}
-		stringdata(std::string s) : mvalue(s[0] != '"' && s[0] != '\'' ? s : parse_from_sqf(s)) {}
-		stringdata(std::string s, bool parse) : mvalue(parse ? parse_from_sqf(s) : s) {}
-		std::string tosqf() const override { return parse_to_sqf(mvalue); }
-		operator const char*() const { return mvalue.c_str(); }
-		operator std::string() const { return mvalue; }
+    class stringdata : public data
+    {
+    private:
+        std::string mvalue;
+    public:
+        stringdata() : mvalue(std::string()) {}
+        stringdata(std::string s) : mvalue(s[0] != '"' && s[0] != '\'' ? s : parse_from_sqf(s)) {}
+        stringdata(std::string s, bool parse) : mvalue(parse ? parse_from_sqf(s) : s) {}
+        std::string tosqf() const override { return parse_to_sqf(mvalue); }
+        operator const char* () const { return mvalue.c_str(); }
+        operator std::string() const { return mvalue; }
 
-		static std::string parse_from_sqf(std::string);
-		static std::string parse_to_sqf(std::string);
-		size_t length() { return mvalue.size(); }
-		bool equals(std::shared_ptr<data> d) const override { return 0 == str_cmpi(mvalue.c_str(), -1, std::dynamic_pointer_cast<stringdata>(d)->mvalue.c_str(), -1); }
-	};
+        static std::string parse_from_sqf(std::string_view);
+        static std::string parse_to_sqf(std::string_view);
+        size_t length() { return mvalue.size(); }
+        bool equals(std::shared_ptr<data> d) const override { return 0 == str_cmpi(mvalue.c_str(), -1, std::dynamic_pointer_cast<stringdata>(d)->mvalue.c_str(), -1); }
+    };
 }
